2f13d22caa3cbc6b4384e5636df4e5d8284d1c8e
fatihturan <eposta@fatihturan.com>
2016-01-31 21:30:11 +0200

    Yeni web sitesi

--- js/main.js
+++ js/main.js
@@ -10,12 +10,30 @@ $(document).ready(function() {
 
 
 
-/*=====================================
-=            HEADER STUFFS            =
-=====================================*/
+/*========================================
+=            INSTAGRAM PHOTOS            =
+========================================*/
+$(document).ready(function() {
+	if (window.Instafeed) {
+		var userFeed = new Instafeed({
+				get: 'user',
+				userId: '1365825',
+				accessToken: '1365825.1bf2ce7.47fe6b5cbf764542a416b5089bfe31d6',
+				template: '<li><a href="{{link}}"><img src="{{image}}" /></a></li>',
+				sortBy: 'random',
+				limit: 10,
+				resolution: 'low_resolution'
+		});
+		userFeed.run();
+	}
+});
 
-// Sticky Header
-$(window).scroll(function() {
+
+
+/*=====================================
+=            HEADER STUFF             =
+=====================================*/
+$(window).scroll(function() { // Sticky Header
 
     if ($(window).scrollTop() > 75) {
         $('.main-nav').addClass('sticky');
@@ -24,24 +42,7 @@ $(window).scroll(function() {
     }
 });
 
-// Medium Like Scroll Up Bar
-$('.main-nav').scrollupbar();
 
-// Smooth Scrolling from: http://css-tricks.com/snippets/jquery/smooth-scrolling/
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
 
 
 
@@ -49,40 +50,30 @@ $(function() {
 =            MAGNIFIC POPUP            =
 ======================================*/
 if(jQuery().magnificPopup) {
-// Add it after jquery.magnific-popup.js and before first initialization code
-$.extend(true, $.magnificPopup.defaults, {
-  tClose: 'Kapat', // Alt text on close button
-  tLoading: 'Yükleniyor...', // Text that is displayed during loading. Can contain %curr% and %total% keys
-  gallery: {
-    tPrev: 'Önceki', // Alt text on left arrow
-    tNext: 'Sonraki', // Alt text on right arrow
-    tCounter: '%curr% / %total%' // Markup for "1 of 7" counter
-  },
-  image: {
-    tError: '<a href="%url%">Dosya</a> yüklenemedi.' // Error message when image could not be loaded
-  },
-  ajax: {
-    tError: '<a href="%url%">İçerik</a> yüklenemedi.' // Error message when ajax request failed
-  }
-});
-
-$('.thumbs li a').click(function(e) {
-  e.preventDefault();
-  var a = this,
-    images = [],
-    data = $(a).data('images').split(',');
-    for(var i in data) {
-      images.push({'src': data[i], title: $(this).attr("data-title")});
-    }
-    $.magnificPopup.open({
-      type: 'image',
-      items: images,
-      gallery: {
-        enabled: true
-      }
-    });
-})
+	$('.featured-works a, .project-thumb a').click(function(e) {
+	  e.preventDefault();
+	  var a = this,
+	    images = [],
+	    data = $(a).data('images').split(',');
+	    for(var i in data) {
+	      images.push({'src': data[i], title: $(this).attr("data-title")});
+	    }
+	    $.magnificPopup.open({
+	      type: 'image',
+	      items: images,
+				image: {
+					cursor: null,
+				},
+	      gallery: {
+	        enabled: true
+	      },
+				closeMarkup: '<button title="%title%" type="button" class="mfp-close"></button>',
+				removalDelay: 300,
+				mainClass: "mfp-fade"
+	    });
+	});
 }
+
 
 
 /*===============================================
@@ -93,12 +84,12 @@ if(jQuery().validate) {
   $('.contact-form').validate({
     errorElement: "span",
       messages: {
-        name: "Lütfen adınızı ve soyadınızı giriniz.",
+        name: "Please type your name",
         email: {
-          required: "Sizinle iletişime geçebilmem için lütfen e-posta adresinizi giriniz.",
-          email: "Yazdığınız e-posta adresi adınız@soyadınız.com şeklinde olmalı."
+          required: "Please type your e-mail address",
+          email: "Please type a valid e-mail address"
         },
-        message: "Lütfen bana ileteceğiniz mesajınızı yazınız."
+        message: "Please type a message"
       }
   });
 }
